7e2d144a827db8012534304c780e34211a1f4dc8
ShaneWaxwing <shane.d.waxwing@gmail.com>
2023-01-17 15:03:00 -0500

    Initial Uno commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from Classes.player import Player
+from Classes.deck import Deck
+
+def main():
+    deck = Deck()
+    players = []
+
+    players.append(Player('Pat-man'))
+    players.append(Player('Bird-boy'))
+
+
+
+
+    print(deck)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
