ae846abd2fcf320590e185ae4846055bd91f8f3b
Shane <shane.d.waxwing@gmail.com>
2023-01-22 21:04:10 -0500

    adding some new functions, getting the game started

---
+++ Classes/cards.py
@@ -0,0 +1,6 @@
+class Card:
+    def __init__(self, color, number):
+        self.color = color
+        self.number = number
+
+        
--- Classes/player.py
+++ Classes/player.py
@@ -1,18 +1,10 @@
 class Player:
     def __init__(self, username):
         self.username = username
-        self.hand = []
-        
-    def __str__(self):
-        return self.username
-
-    def __repr__(self):
-        return self.username
-
 
 
     def play_turn():
         pass
 
 
-    
+    hand = []
